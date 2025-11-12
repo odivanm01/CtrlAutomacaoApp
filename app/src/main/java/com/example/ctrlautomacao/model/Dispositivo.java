63b5bf2cded5574d7ba12df4806e418768f4f25b
odivanm01 <odivan.m01@aluno.ifsc.edu.br>
2025-11-12 17:06:29 -0300

    v.20251112

--- app/src/main/java/com/example/ctrlautomacao/model/Dispositivo.java
+++ app/src/main/java/com/example/ctrlautomacao/model/Dispositivo.java
@@ -1,13 +1,62 @@
 package com.example.ctrlautomacao.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Dispositivo {
     //atributos
+    private int id;
     private String estado;
     private String grupo;
     private String tipo;
     private int idAmbiente;
 
+    //construtores
+    public Dispositivo() {
+        this.id = 0;
+        this.estado = "";
+        this.grupo = "";
+        this.tipo = "";
+        this.idAmbiente = 0;
+    }
+
+    //CONSTRUTOR - inicializa atributos de um arquivo JSon
+    public Dispositivo (JSONObject jp) {
+        try {
+            this.setId(jp.getInt("ipDispositivo"));
+            this.setEstado(jp.getString("Estado"));
+            this.setGrupo(jp.getString("Grupo"));
+            this.setTipo(jp.getString("Tipo"));
+            this.setIdAmbiente(jp.getInt("idAmbiente"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Metodo retorna o objeto com dados no formato JSON
+    public JSONObject toJsonObject() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("ipDispositivo", this.id);
+            json.put("Estado", this.estado);
+            json.put("Grupo", this.grupo);
+            json.put("Tipo", this.tipo);
+            json.put("idAmbienrte", this.idAmbiente);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
     //metodos
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getEstado() {
         return estado;
