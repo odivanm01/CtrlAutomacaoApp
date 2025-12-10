2641ffc450f8b3b7fadcabcb1f3464425eb81808
odivanm01 <odivan.m01@aluno.ifsc.edu.br>
2025-12-10 16:48:41 -0300

    v1.20251210

--- app/src/main/java/com/example/ctrlautomacao/ui/dispositivo/ConDispositivoFragment.java
+++ app/src/main/java/com/example/ctrlautomacao/ui/dispositivo/ConDispositivoFragment.java
@@ -12,19 +12,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.ctrlautomacao.R;
 import com.example.ctrlautomacao.model.Dispositivo;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 /**
  * A fragment representing a list of Items.
  */
-public class ConDispositivoFragment extends Fragment implements Response.ErrorListener, Response.Listener{
+public class ConDispositivoFragment extends Fragment implements Response.ErrorListener, Response.Listener<JSONArray>{
 
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
@@ -32,7 +39,7 @@ public class ConDispositivoFragment extends Fragment implements Response.ErrorLi
     private int mColumnCount = 1;
 
     //Lista que vai armazenar os objetos que retornam do Web Service
-    private ArrayList<Dispositivo> usuarios;
+    private ArrayList<Dispositivo> dispositivos;
     //Fila de requests da biblioteca Volley
     private RequestQueue requestQueue;
     //Objeto da biblioteca Volley que faz o request para o Web Service
@@ -70,18 +77,81 @@ public class ConDispositivoFragment extends Fragment implements Response.ErrorLi
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         this.view = inflater.inflate(R.layout.fragment_con_dispositivo_list, container, false);
-
-
+        //
+        //instanciando a fila de requests - caso o objeto seja o view
+        this.requestQueue = Volley.newRequestQueue(view.getContext());
+        //inicializando a fila de requests do SO
+        this.requestQueue.start();
+        //
+        //requisição para o Rest Server tipo POST
+        try {
+            jsonArrayReq = new JsonArrayRequest(
+                    Request.Method.POST,
+                    "http://10.0.2.2/php/conDispositivo.php",
+                    new JSONArray("[{}]"),
+                    this, this);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        //Inclui a request na fila
+        requestQueue.add(jsonArrayReq);
         return this.view;
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-
+        //mostrar mensagem que veio do servidor
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar a consulta: " +
+                        error.toString(), Snackbar.LENGTH_LONG);
+        mensagem.show();
     }
 
     @Override
-    public void onResponse(Object response) {
+    public void onResponse(JSONArray jsonArray) {
+        try {
+            //se a consulta não veio vazia passar para array list
+            if (jsonArray != null) {
+                //objeto java
+                Dispositivo dispositivo = null;
+                //array list para receber a resposta
+                this.dispositivos = new ArrayList<Dispositivo>();
+                //preenchendo ArrayList com JSONArray recebido
+                for (int pos=0;pos<jsonArray.length();pos++) {
+                    JSONObject jo = jsonArray.getJSONObject(pos);
+                    dispositivo = new Dispositivo(jo);
+                    this.dispositivos.add(dispositivo);
+                }
+                /*
+                O codigo abaixo já estava no metodo onCreateView().
+                Mas foi movido para ca, porque so pode ser
+                executado se o ArrayList não estiver vazio.
+                */
+                if (view instanceof RecyclerView) {
+                    Context context = view.getContext();
+                    RecyclerView recyclerView =
+                            (RecyclerView) view;
+                    if (mColumnCount <= 1) {
+                        recyclerView.setLayoutManager(
+                                new LinearLayoutManager(context));
+                    } else {
+                        recyclerView.setLayoutManager(
+                                new GridLayoutManager(context,
+                                        mColumnCount));
+                    }
+                    recyclerView.setAdapter(
+                            new DispositivoRecyclerViewAdapter(this.dispositivos));
+                }
+            }else {
+                Snackbar mensagem = Snackbar.make(view,
+                        "A consulta não retornou nenhum registro!",
+                        Snackbar.LENGTH_LONG);
+                mensagem.show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
